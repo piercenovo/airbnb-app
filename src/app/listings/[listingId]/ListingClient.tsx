edfd5e8978b1cb39f034b3035a5b433744b6f336
piercenovo <piercenovoah@gmail.com>
2023-04-09 20:12:18 -0500

    feat: add listing reservation component

--- src/app/listings/[listingId]/ListingClient.tsx
+++ src/app/listings/[listingId]/ListingClient.tsx
@@ -1,12 +1,27 @@
 'use client'
 
+import axios from 'axios'
+import { toast } from 'react-hot-toast'
+import { differenceInCalendarDays, eachDayOfInterval } from 'date-fns'
+import { Range } from 'react-date-range'
+
 import { Container } from '@/components/Container'
 import { ListingHead } from '@/components/Listings/ListingHead'
 import { ListingInfo } from '@/components/Listings/ListingInfo'
 import { categories } from '@/data/categories'
-import { type SafeListing, type SafeUser } from '@/types'
+import { ListingReservation } from '@/components/Listings/ListingReservation'
 import { Reservation } from '@prisma/client'
-import { useMemo } from 'react'
+import { type SafeListing, type SafeUser } from '@/types'
+
+import { useLoginModal } from '@/hooks/useLoginModal'
+import { useRouter } from 'next/navigation'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: 'selection'
+}
 
 interface ListingClientProps {
   reservations?: Reservation[]
@@ -17,9 +32,82 @@ interface ListingClientProps {
 }
 
 export const ListingClient: React.FC<ListingClientProps> = ({
+  reservations = [],
   listing,
   currentUser
 }) => {
+  const loginModal = useLoginModal()
+  const router = useRouter()
+
+  const disabledDates = useMemo(() => {
+    let dates: Date[] = []
+
+    reservations.forEach((reservation) => {
+      const range = eachDayOfInterval({
+        start: new Date(reservation.startDate),
+        end: new Date(reservation.endDate)
+      })
+
+      dates = [...dates, ...range]
+    })
+
+    return dates
+  }, [reservations])
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [totalPrice, setTotalPrice] = useState(listing.price)
+  const [dateRange, setDateRange] = useState<Range>(initialDateRange)
+
+  const onCreateReservation = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen()
+    }
+
+    setIsLoading(true)
+
+    axios.post('/api/reservations', {
+      totalPrice,
+      startDate: dateRange.startDate,
+      endDate: dateRange.endDate,
+      listingId: listing?.id
+    })
+      .then(() => {
+        toast.success('Listing reserved!')
+        setDateRange(initialDateRange)
+
+        // Redirect to /trips
+        router.refresh()
+      })
+      .catch(() => {
+        toast.error('Something went wrong')
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, [
+    totalPrice,
+    dateRange,
+    listing?.id,
+    router,
+    currentUser,
+    loginModal
+  ])
+
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      const dayCount = differenceInCalendarDays(
+        dateRange.endDate,
+        dateRange.startDate
+      )
+
+      if (dayCount && listing.price) {
+        setTotalPrice(dayCount * listing.price)
+      } else {
+        setTotalPrice(listing.price)
+      }
+    }
+  }, [dateRange, listing.price])
+
   const category = useMemo(() => {
     return categories.find((item) => item.label === listing.category)
   }, [listing.category])
@@ -45,6 +133,24 @@ export const ListingClient: React.FC<ListingClientProps> = ({
               bathroomCount={listing.bathroomCount}
               locationValue={listing.locationValue}
             />
+            <div
+              className='
+              order-first
+              mb-10
+              md:col-span-3
+              md:order-last
+              '
+            >
+              <ListingReservation
+                price={listing.price}
+                totalPrice={totalPrice}
+                onChangeDate={(value: any) => setDateRange(value)}
+                dateRange={dateRange}
+                onSubmit={onCreateReservation}
+                disabled={isLoading}
+                disabledDates={disabledDates}
+              />
+            </div>
           </div>
         </div>
       </div>
