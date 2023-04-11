a6c3b1836868752cff24083b5c496a880c236d6a
piercenovo <piercenovoah@gmail.com>
2023-04-11 01:29:58 -0500

    feat: add loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+export function Loader () {
+  return (
+    <div
+      className='
+      h-[70vh]
+      flex
+      flex-col
+      justify-center
+      items-center
+      '
+    >
+      <span className='loader' />
+    </div>
+  )
+}
